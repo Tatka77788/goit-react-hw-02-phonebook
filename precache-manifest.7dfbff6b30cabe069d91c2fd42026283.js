5de232e5b23774cf6a94b1536ccb472914cc3c1f
Tetiana <tatka777t@gmail.com>
2020-04-09 12:48:35 +0300

    Updates

---
+++ precache-manifest.7dfbff6b30cabe069d91c2fd42026283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2d5cbb5824eca5fa39bac8609d7380",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c45f1ba89625b18e0836",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.23f7a8d8.chunk.css"
+  },
+  {
+    "revision": "34cd76bd64eb0919e0fe",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.04da34af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.04da34af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45f1ba89625b18e0836",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.ef92f3b5.chunk.js"
+  },
+  {
+    "revision": "4dc4691f794833de7ba9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.bd2cb211.js"
+  }
+]);
